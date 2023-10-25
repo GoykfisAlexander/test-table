a297e66c895b71495c71601a6fe0baed013401fe
GoykfisAlexander <agoykhfis@gmail.com>
2023-10-25 15:24:51 +0300

    Updates

---
+++ precache-manifest.fca5042e30c1b6197adfaf9f145e6da7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62346330e45fda4bfe59817ec34a363c",
+    "url": "/test-table/index.html"
+  },
+  {
+    "revision": "8e8fb4b4ceb9ba75866d",
+    "url": "/test-table/static/css/2.6f2db2d8.chunk.css"
+  },
+  {
+    "revision": "b6f172d6b5a3938c197a",
+    "url": "/test-table/static/css/main.58ca551a.chunk.css"
+  },
+  {
+    "revision": "8e8fb4b4ceb9ba75866d",
+    "url": "/test-table/static/js/2.3fe663ba.chunk.js"
+  },
+  {
+    "revision": "279e4fca54e5d8e57f31",
+    "url": "/test-table/static/js/3.33a557c1.chunk.js"
+  },
+  {
+    "revision": "b6f172d6b5a3938c197a",
+    "url": "/test-table/static/js/main.96cc8282.chunk.js"
+  },
+  {
+    "revision": "93d1637505cd924bcaec",
+    "url": "/test-table/static/js/runtime~main.20a9afad.js"
+  }
+]);
